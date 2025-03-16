2845b25fb1baec98132fdd6d985558bf91107557
5lwf <75666989+khaflia@users.noreply.github.com>
2025-03-16 15:27:27 +0400

    Create  webserver.py

---
+++ webserver.py
@@ -0,0 +1,12 @@
+from flask import Flask
+efrom threading import Thread
+app Flask('')
+@app.route('/').
+def home():
+return "Discord bot ok"
+3 usages (2 dynamic)
+def runO:
+app.run(host="0.0.0.0", port=
+def keep alive():
+t = Thread (target=run)
+t.start
